cac8cc013f19052819558fd1414a1ed82afc2311
IlanIwumbwe <ikibfm@gmail.com>
2024-05-22 12:21:42 +0100

    fixing random data bugs and making class for easy reuse

--- data/datavis.py
+++ data/datavis.py
@@ -4,49 +4,40 @@ import math
 import matplotlib.pyplot as plt
 
 # data for 10 cycles
-cycles = {0:[], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[]}
+cycles = {0:[[], [], []], 1:[[], [], []], 2:[[], [], []], 3:[[], [], []], 4:[[], [], []], 5:[[], [], []], 6:[[], [], []], 7:[[], [], []], 8:[[], [], []], 9:[[], [], []]}
 
-cycle_time = 300  # 5 minutes
-random_time = 5   # 5 seconds for new random data
+class Data:
+    def __init__(self) -> None:
+        self.cycle_time = 300  # 5 minutes
+        self.random_time = 5   # 5 seconds for new random data
+        self.data_points = self.cycle_time // self.random_time
+        self.sines = [np.sin(((i+1)/30)*math.pi) for i in range(self.data_points)]
 
-data_points = cycle_time // random_time
+    # randomise all cycles
+    def randomise(self):    
+        for num, cycle in cycles.items():
+            for a in cycle:
+                a.clear()
+                for i in range(self.data_points):
+                    a.append(round(random.randrange(0, 101, 2) + 10*self.sines[i], 2))
 
-import_costs = []
-export_costs = []
-irradiance_vals = []
+    def print_data(self):
+        for cycle in cycles.values():
+            print(cycle)
 
-sines = [np.sin(((i+1)/30)*math.pi) for i in range(data_points)]
+    def plot_data(self, data_index):
+        times = [i*5 for i in range(self.data_points)]
+        figure, axis = plt.subplots(2, len(cycles) // 2)
 
-# randomise array with 60 random values to simulate a day / cycle
-# spec says there's a repeating periodic component as well, so I have got my values from sin graph
-def randomise(input_arrays):    
-    for a in input_arrays:
-        a.clear()
-        for i in range(data_points):
-            a.append(round(random.randrange(0, 101, 2) + 10*sines[i], 2))
+        for row in range(2):
+            for col in range(len(cycles) // 2):
+                cycle_num = 5*row + col
+                axis[row, col].plot(times, cycles[cycle_num][data_index])
+                axis[row, col].set_title(f"Cycle number {cycle_num}")
+            
+        plt.show()
 
-# 0 = import, 1 = export, 2 = irrandiance
-def populate_data():
-    for num, _ in cycles.items():
-        randomise([import_costs, export_costs, irradiance_vals])
-        cycles[num] = [import_costs, export_costs, irradiance_vals]
-
-def print_data():
-    for cycle in cycles.values():
-        print(cycle)
-
-def plot_data(data_index):
-    times = [i*5 for i in range(data_points)]
-    figure, axis = plt.subplots(2, len(cycles) // 2)
-
-    for row in range(2):
-        for col in range(len(cycles) // 2):
-            cycle_num = 5*row + col
-            axis[row, col].plot(times, cycles[cycle_num][data_index])
-            axis[row, col].set_title(f"Cycle number {cycle_num}")
-        
-    plt.show()
-
-populate_data()
-print_data()
-plot_data(0)
+if __name__ == "__main__":
+    d = Data()
+    d.randomise()
+    d.plot_data(0)
