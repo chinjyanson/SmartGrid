3af9971588781619168dde44786f63c23580294b
chinjyanson <anson.chin22@imperial.ac.uk>
2024-05-22 23:57:51 +0100

    [ayc122] <feat> working on ILP for minimizing loss algo

--- buy-sell-algorithm/scheduling.py
+++ buy-sell-algorithm/scheduling.py
@@ -8,6 +8,10 @@ num_time_slots = 10
 use = pulp.LpVariable.dicts("use", ((j, i) for j in range(num_jobs) for i in range(num_time_slots)), 
                             lowBound=0, cat='Integer')
 
+# Binary variables to indicate if a job is active at a given time slot
+active = pulp.LpVariable.dicts("active", ((j, i) for j in range(num_jobs) for i in range(num_time_slots)),
+                               cat='Binary')
+
 # Length of each job
 Time = [3, 2, 4, 1, 2]  # Example job lengths
 
@@ -24,11 +28,14 @@ prob += pulp.lpSum([use[j, i] for j in range(num_jobs) for i in range(num_time_s
 for i in range(num_time_slots):
     # Constraint: Total energy used at time slot i should not exceed the capacity
     prob += pulp.lpSum([use[j, i] for j in range(num_jobs)]) <= capacity[i], f"Capacity_Constraint_{i}"
+    
+    # Constraint: No more than 3 tasks running at the same time slot
+    prob += pulp.lpSum([active[j, i] for j in range(num_jobs)]) <= 3, f"Task_Limit_Constraint_{i}"
 
 for j in range(num_jobs):
-    for i in range(num_time_slots - Time[j] + 1):
-        # Constraint: Energy used by job j should not exceed the length of the job times the capacity
-        prob += pulp.lpSum([use[j, k] for k in range(i, i + Time[j])]) <= Time[j] * min(capacity[i:i + Time[j]]), f"Job_Length_Constraint_{j}_{i}"
+    for i in range(num_time_slots):
+        # Ensure that if a job is active, it is marked as such in the active variable
+        prob += use[j, i] <= capacity[i] * active[j, i], f"Active_Use_Link_{j}_{i}"
 
 # Solve the problem
 prob.solve()
