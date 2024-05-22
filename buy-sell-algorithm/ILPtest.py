d31a67b10b1915d0e006555106f4b39d1332f350
chinjyanson <anson.chin22@imperial.ac.uk>
2024-05-22 11:42:18 +0100

    [ayc122] <test> added ARIMA modules and begin testing ILP for scheduling and resource allocation

---
+++ buy-sell-algorithm/ILPtest.py
@@ -0,0 +1,10 @@
+
+
+class Scheduling:
+    def __init__ (self, use, time, capacity):
+        self.use = use
+        self.time = time
+        self.capacity = capacity
+
+    def find_optimal():
+        for 
