ef46bdddbdba6b64fc25318ce1aee4f6b7428e9a
IlanIwumbwe <ikibfm@gmail.com>
2024-05-23 11:46:48 +0100

    parsing everything into one parsed data dict

--- data/server_data.py
+++ data/server_data.py
@@ -9,26 +9,43 @@ class server_data:
         self.url = 'https://icelec50015.azurewebsites.net'
         self.json = ""
         self.data_points = 60
+        self.parsed_data = {'buy_price':[], 'demand':[], 'sell_price':[], 'sun':[], 'deferables':[]}
+        self.deffs = []
 
     def set_json(self, endpoint):
         print(self.url+endpoint)
         self.json = requests.get(self.url+endpoint).json()
-        print(self.json)
 
-    def get_historical_prices(self):
-        out = {'buy_price':[], 'demand':[], 'sell_price':[]}
+    def set_historical_prices(self):
+        self.set_json('/yesterday')
 
         for s in ['buy_price', 'demand', 'sell_price']:
             for i in range(self.data_points):
-                out[s].append(self.json[i][s])
+                self.parsed_data[s].append(self.json[i][s])
+    
+    def live_sunshine(self):
+        self.set_json('/sun')
+        self.parsed_data['sun'] = self.set_json['sun']
 
-        return out
+    def live_prices(self):
+        self.set_json('/price')
+        self.parsed_data['buy_price'] = self.set_json['buy_price']
+        self.parsed_data['sell_price'] = self.set_json['sell_price']
+
+    def live_demand(self):
+        self.set_json('/demand')
+        self.parsed_data['demand'] = self.set_json['demand']
+
+    def deferables(self):
+        self.set_json('/deferables')
+        self.parsed_data['deferables'] = self.json  
 
 if (__name__ == "__main__"):
     serve = server_data()
-    serve.set_json('/yesterday')
+    serve.set_historical_prices()
 
-    buy = serve.get_historical_prices()['buy_price']
-    sell = serve.get_historical_prices()['sell_price']
+    buy = serve.parsed_data['buy_price']
+    sell = serve.parsed_data['sell_price']
 
-    utils.plot_datas(sell, None)
+    serve.deferables()
+    print(serve.parsed_data['deferables'])
