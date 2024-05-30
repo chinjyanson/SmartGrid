b63d24b5a5f844942178cef8f4eee91654eb30fd
Samuel Khoo <115562183+SamuelKhoo2003@users.noreply.github.com>
2024-05-30 16:54:55 +0100

    [ssk122]: Finalised visualisers and API testing

---
+++ aws-backend/python-test-scripts/visualisers/energyUsageVisualiser.py
@@ -0,0 +1,27 @@
+import requests
+from tabulate import tabulate
+
+import matplotlib.pyplot as plt
+
+response = requests.get('https://evuc3y0h1g.execute-api.eu-north-1.amazonaws.com/PROD/accessUsageLog')
+if response.status_code != 200:
+    print('Error: ', response.status_code)
+    exit()
+
+data = response.json()
+data.sort(key=lambda item: item['dayID'])
+day = [item['dayID'] for item in data]
+usage = [item['energyUsed'] for item in data]
+
+plt.plot(day, usage, label='Energy Used')
+plt.xlabel('Day')
+plt.ylabel('Energy Used')
+plt.title('Day VS Energy Used')
+plt.legend()
+plt.show()
+
+table_data = []
+for item in data:
+    table_data.append([item['dayID'], item['energyUsed']])
+
+print(tabulate(table_data, headers=['Day', 'Energy Used']))
