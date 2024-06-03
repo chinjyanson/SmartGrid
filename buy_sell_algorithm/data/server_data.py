3bd6d26454b99dc0e6a8d1ea3849de72cc0134bc
IlanIwumbwe <ikibfm@gmail.com>
2024-06-03 12:17:15 +0100

    adding Anson's function call into main algorithm driver, reducing timeout limit

--- buy_sell_algorithm/data/server_data.py
+++ buy_sell_algorithm/data/server_data.py
@@ -55,7 +55,7 @@ class server_data:
     
     async def create_session(self):
         if(not self.session or self.session.closed):
-            timeout = aiohttp.ClientTimeout(total=1)
+            timeout = aiohttp.ClientTimeout(total=0.5)
             self.session = aiohttp.ClientSession(timeout=timeout)
 
     async def close_session(self):
