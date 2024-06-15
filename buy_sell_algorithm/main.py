4a70e1bc554bdd14513804d02d965f3e8257d749
chinjyanson <anson.chin22@imperial.ac.uk>
2024-06-16 00:04:47 +0100

    [ayc122] <feat> functional tcp_bidirectional and changed for fake data main

--- buy_sell_algorithm/main.py
+++ buy_sell_algorithm/main.py
@@ -3,7 +3,7 @@ from threading import Thread
 #from algo_driver import Algorithm
 from queue import Queue 
 
-m_tcp = module_from_file("run_server", "tcp/tcp_server_II.py")
+m_tcp = module_from_file("run_server", "tcp/tcp_server.py")
 a_tcp = module_from_file("driver", "tcp/fake_algo.py")
 q = Queue()
 
@@ -12,9 +12,12 @@ def main():
     #init_frontend_file()
 
     #algo = Algorithm()
+    server_host = '0.0.0.0'
+    server_port = 5555  
+
 
     algo_thread = Thread(target=a_tcp.driver, args=(q, ))
-    tcp_thread = Thread(target=m_tcp.run_server, args=(q, ))
+    tcp_thread = Thread(target=m_tcp.start_server, args=(server_host, server_port, q, ))
 
     algo_thread.start()
     tcp_thread.start()
